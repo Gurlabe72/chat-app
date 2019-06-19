6475f1ce24b8123044b6a95a7a7f9859c2b8a68c
Gurlabe <Gurlabe72@gmail.com>
2019-06-19 16:30:53 -0700

    App.js chatkit configuration

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
-import React from 'react';
-import './App.css';
-import MessageList from "./components/MessageList"
+import React from 'react'
+import Chatkit from '@pusher/chatkit'
+import MessageList from './components/MessageList'
+import SendMessageForm from './components/SendMessageForm'
+import RoomList from './components/RoomList'
+import NewRoomForm from './components/NewRoomForm'
 
-function App() {
-  return (
-    <div className="App">
-      <MessageList />
-    </div>
-  );
+import { tokenUrl, instanceLocator } from './config'
+
+class App extends React.Component {
+
+  componentDidMount () {
+    const chatManager = new Chatkit.ChatManager({
+            instanceLocator: instanceLocator,
+            userId: "John Doe",
+            tokenProvider: new Chatkit.TokenProvider({
+                url: tokenUrl
+            })
+    })
+    
+    chatManager.connect()
+    .then(currentUser => {
+        currentUser.subscribeToRoom({
+          roomId: 19425969,
+          //the amount of messgaes can be limited
+          messageLimit: 50,
+          hooks: {
+            onNewMessage: message => {
+              console.log('message.text: ', message.text);
+            }
+          }
+        })
+    })
+  }
+  render() {
+    return (
+      <div className="app">
+        <RoomList />
+        <MessageList />
+        <SendMessageForm />
+        <NewRoomForm />
+      </div>
+    )
+  }
 }
 
-export default App;
+
+      
+
+
+  
+
+export default App
---
+++ src/config.js
@@ -0,0 +1,6 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/9507b826-01ec-4a0a-aa0d-4de80e57802a/token";
+const instanceLocator = "v1:us1:9507b826-01ec-4a0a-aa0d-4de80e57802a";
+
+exports.tokenUrl = tokenUrl;
+exports.instanceLocator = instanceLocator;
+
